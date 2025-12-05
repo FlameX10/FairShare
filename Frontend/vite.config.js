bd3f1460618600adc6054315ee7840cfb85f4fe0
FlameX10 <150046965+FlameX10@users.noreply.github.com>
2025-12-05 17:05:54 +0530

    v2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: "/",    // <-- REQUIRED for Netlify / production
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
   },

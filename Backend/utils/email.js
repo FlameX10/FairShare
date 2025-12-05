a88b894c8a3769dc68b83a619e11f518ce97159f
FlameX10 <150046965+FlameX10@users.noreply.github.com>
2025-12-05 20:43:50 +0530

    b

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -1,28 +1,9 @@
-// import nodemailer from "nodemailer";
-
-// export const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS
-//   }
-// });
-
-
-
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,      // smtp-relay.brevo.com
-  port: process.env.SMTP_PORT,      // 587
-  secure: false,                    // MUST be false for port 587
+  service: "gmail",
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,      // REQUIRED on Render
-  }
 });
